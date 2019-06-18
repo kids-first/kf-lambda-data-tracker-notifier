940b1929b028277281e0301b975523024afee3d2
Dan Kolbman <dankolbman@gmail.com>
2019-06-18 10:03:51 -0400

    :arrow_right: Route Notifications button to profile

--- service.py
+++ service.py
@@ -238,7 +238,9 @@ class UserNotifier:
         """
         Send a user notifications for their interested studies
         """
-        summary = summary_header(self.format_url(self.url), self.date)
+        summary = summary_header(
+            self.format_url(f"{self.url}/profile"), self.date
+        )
         blocks = [summary]
 
         for study in user["studySubscriptions"]["edges"]:
